fe9c3a3b519f2dbe6314123e7fd23bc73b4a4990
jasmineirizarry <72569201+jasmineirizarry@users.noreply.github.com>
2020-12-07 17:41:32 -0600

    Add files via upload

---
+++ class object.py
@@ -0,0 +1,8 @@
+#lab week 8 part 2
+class yarn:
+    price: 8.99
+    weight= 1.2lbs
+    delivery_time= 2
+    size "400 yards"
+    name= red heart soft yarn(color: blush rose)
+
---
+++ item.py
@@ -0,0 +1,14 @@
+#lab week 8 part 2
+class item:
+    price= 0
+    weight=0
+    delivery time=0
+    size=""
+    name=""
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        
+        
+    
